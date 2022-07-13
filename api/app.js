0c83ec22c79efd49a8d827cf4efa4efd7f34032b
flportilla <flportilla@gmail.com>
2022-07-13 18:29:30 -0500

    Add user routes

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 
 const todoRouter = require('./controllers/todo')
+const usersRouter = require('./controllers/user')
 const config = require('./utils/config')
 
 const mongoose = require('mongoose')
@@ -21,5 +22,6 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use('/api/todos', todoRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
