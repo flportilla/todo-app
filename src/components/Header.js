8e119d0ac2e4773e2e621e5a873222ae850d039e
flportilla <flportilla@gmail.com>
2022-07-13 11:24:53 -0500

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../style/header.css'
+
+const Header = ({ handleSearch }) => {
+
+  const clearSearch = (e) => e.target.parentElement.firstChild.lastChild.value = ''
+
+  return (
+    <div className='header_container'>
+      <a href='http://localhost:3000/'>Home</a>
+      <form>
+        <label htmlFor='searchBar'>
+          Search
+          <input
+            id='searchBar'
+            type='text'
+            onChange={(e) => handleSearch(e)}
+          />
+        </label>
+        <button
+          type='button'
+          onClick={clearSearch}
+        >
+          Clear
+        </button>
+      </form>
+      <div> """""login credentials""""" </div>
+    </div>
+  )
+}
+
+export default Header
