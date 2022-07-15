b1fb4850e962ec5c2df69fbf6216d2c6920a7a4c
Fabian Portilla <fas_456@hotmail.com>
2022-07-15 10:57:10 -0500

    Add new user link

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,32 @@
 import React from 'react'
 
 const Login = () => {
+
+
   return (
-    <form className='login_form'>
-      <label htmlFor='user_name'>
-        User
-      </label>
-      <input
-        className='login_user'
-        type={'text'} />
-      <label htmlFor='user_name'>
-        Password
-      </label>
-      <input
-        className='login_password'
-        type={'password'} />
-      <button type='submit'>login</button>
-    </form >
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      margin: "5px",
+    }}>
+      <form className='login_form'>
+        <label htmlFor='user_name'>
+          User
+        </label>
+        <input
+          className='login_user'
+          type={'text'} />
+        <label htmlFor='user_name'>
+          Password
+        </label>
+        <input
+          className='login_password'
+          type={'password'} />
+        <button type='submit'>login</button>
+      </form >
+      <a href='/'>new user? click here</a>
+    </div>
   )
 }
 
