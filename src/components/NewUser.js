6ea3840c91a5448b31ed0c634826499fd3475799
Fabian Portilla <fas_456@hotmail.com>
2022-07-17 10:09:30 -0500

    Create basic states to handle form values

--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -1,47 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../style/newUser.css'
 
 const NewUser = ({ isDisplayed }) => {
+  const [newUser, setNewUser] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [newName, setNewName] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+
+  const handeUserCreation = (e) => {
+    e.preventDefault();
+    console.log('hello')
+  }
+
   return (
     <div className={isDisplayed ? 'new_user_form_container' : 'new_user_form_container hide'}>
-      <form className='new_user_form'>
+      <form
+        className='new_user_form'
+        onSubmit={handeUserCreation}
+      >
         <label htmlFor='username'>
-          New Username
+          New Username*
         </label>
         <input
+          value={newUser}
+          onChange={({ target }) => setNewUser(target.value)}
+          placeholder='Username'
           name='username'
           id='username'
           type={'text'}
-          minLength={5}
+          minLength={3}
           required
         />
         <label htmlFor='name'>
-          Name
+          Name*
         </label>
         <input
+          value={newName}
+          onChange={({ target }) => setNewName(target.value)}
+          placeholder='Name'
           name='name'
           id='name'
           type={'text'}
-          minLength={5}
+          minLength={3}
           required
         />
         <label htmlFor='password'>
-          New Password
+          New Password*
         </label>
         <input
+          value={newPassword}
+          onChange={({ target }) => setNewPassword(target.value)}
+          placeholder='Password'
           name='password'
           id='password'
           type={'password'}
-          minLength={5}
+          minLength={3}
           required
         />
         <label htmlFor='password'>
-          Repeat password
+          Repeat password*
         </label>
         <input
+          value={repeatPassword}
+          onChange={({ target }) => setRepeatPassword(target.value)}
+          placeholder='Repeat password'
           name='password'
           type={'password'}
-          minLength={5}
+          minLength={3}
           required
         />
         <button
