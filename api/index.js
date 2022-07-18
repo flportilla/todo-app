e484712b018a96e17e7ced317babdbf841719994
Fabian Portilla <fas_456@hotmail.com>
2022-07-18 10:14:37 -0500

    ---

--- api/index.js
+++ api/index.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 const app = require('./app') // the actual Express application
 const http = require('http')
 const config = require('./utils/config')
-const Todo = require('./models/todos')
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
