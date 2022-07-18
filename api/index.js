1a60bb45cea5f2db49ea73bc5071cab7bb585f35
Fabian Portilla <fas_456@hotmail.com>
2022-07-18 09:48:06 -0500

    Modify the env variables to support Heroku

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const app = require('./app') // the actual Express application
 const http = require('http')
 const config = require('./utils/config')
+const Todo = require('./models/todos')
 
 const server = http.createServer(app)
 
-server.listen(config.PORT, () => {
+server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
